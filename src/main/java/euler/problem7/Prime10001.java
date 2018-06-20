b1315cbd562061ddbdb64d3b621c1f9d677b45fa
Zimon Kuhs <zmk.zk.dev@gmail.com>
2018-06-20 16:38:26 +0200

    feat(euler): Solves problem 7.

---
+++ src/main/java/euler/problem7/Prime10001.java
@@ -0,0 +1,19 @@
+package euler.problem7;
+
+import euler.util.Algorithms;
+
+public class Prime10001 {
+
+    public static void main(String[] args) {
+        long x = 1;
+        int nPrimes = 1;
+        while (nPrimes < 10001) {
+            ++x;
+            if (Algorithms.isPrime(x)) {
+                ++nPrimes;
+            }
+        }
+
+        System.out.println(x);
+    }
+}
