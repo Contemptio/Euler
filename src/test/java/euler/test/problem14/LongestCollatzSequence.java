7ea0839653ccef6be770a0cf4124d87b1894a0d7
Contemptio <zmk.zk.dev@gmail.com>
2019-10-10 00:16:31 +0200

    feat(LargestCollatzSequence): Adds problem 14 skeleton.

---
+++ src/test/java/euler/test/problem14/LongestCollatzSequence.java
@@ -0,0 +1,18 @@
+package euler.test.problem14;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LongestCollatzSequence {
+
+    @Test
+    public void problem14() {
+
+        /*
+         * TODO: Implement.
+         */
+
+        assertEquals(0, 1);
+    }
+}
