91c0f02d605a0507289fffe30ad7874cf7c1ce64
Contemptio <zmk.zk.dev@gmail.com>
2019-10-10 01:08:32 +0200

    feat(LatticePaths): Adds skeleton for problem 15.

---
+++ src/test/java/euler/test/problem15/LatticePaths.java
@@ -0,0 +1,17 @@
+package euler.test.problem15;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LatticePaths {
+
+    @Test
+    public void problem15() {
+        /*
+         * TODO: Implement.
+         */
+
+        assertEquals(0, 1);
+    }
+}
