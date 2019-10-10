6baa557690d2254c235d361eff20b5383b601b12
Contemptio <zmk.zk.dev@gmail.com>
2019-10-10 16:29:38 +0200

    feat(NumberLetterCounts): Adds skeleton for problem 17.

---
+++ src/test/java/euler/test/problem17/NumberLetterCounts.java
@@ -0,0 +1,18 @@
+package euler.test.problem17;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import euler.test.ProblemBase;
+
+public class NumberLetterCounts extends ProblemBase {
+
+    @Test
+    public void problem17() {
+        /*
+         * TODO: Implement.
+         */
+        assertEquals(0, 1);
+    }
+}
